fe16892962c5968c2ee8ee53ca6b0f7be7afc03a
Harsh <resmanage150@gmail.com>
2019-03-22 16:42:37 +0530

    parking needs correction

---
+++ app/src/main/java/com/example/user_registeration/Show_Timings_Dialog.java
@@ -0,0 +1,13 @@
+package com.example.user_registeration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Show_Timings_Dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show__timings__dialog);
+    }
+}
