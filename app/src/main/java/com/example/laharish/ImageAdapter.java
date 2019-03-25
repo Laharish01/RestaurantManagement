4d0237a088104dbe27f4ab0502cd26e625a6c414
ReataurantManagement <resmanage150@gmail.com>
2019-03-25 18:08:48 +0530

    lol

---
+++ app/src/main/java/com/example/laharish/ImageAdapter.java
@@ -0,0 +1,106 @@
+package com.example.laharish;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.laharish.Interface.ItemClickListener;
+import com.example.laharish.Model.Category;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<MenuViewHolder> {
+
+    private static final String TAG = "ImageAdapter";
+
+    private Context mContext;
+    private List<Category> menu= new ArrayList<Category>();
+
+    ItemClickListener clickListener;
+
+    public ImageAdapter(Context context, List<Category> menus){
+        mContext=context;
+        menu=menus;
+    }
+
+
+    @NonNull
+    @Override
+    public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v= LayoutInflater.from(mContext).inflate(R.layout.menu_item, viewGroup, false);
+        return new MenuViewHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MenuViewHolder menuViewHolder, final int i) {
+
+        Log.d(TAG, "onBindViewHolder  : called");
+        final Category mmenu=menu.get(i);
+        menuViewHolder.txtMenu.setText(mmenu.getName());
+        Picasso.with(mContext).load(mmenu.getImage())
+                .fit()
+                .centerCrop()
+                .into(menuViewHolder.img);
+
+        menuViewHolder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "OnClick : clicked on : " + mmenu.getImage());
+                String name = mmenu.getName();
+                Toast.makeText(mContext, ""+ name, Toast.LENGTH_SHORT).show();
+                Intent foodList = new Intent(mContext, FoodList.class);
+                foodList.putExtra("Name" , name);
+                Log.d(TAG, "onClick: clicked" + name);
+                mContext.startActivity(foodList);
+            }
+        });
+
+    }
+    @Override
+    public int getItemCount() {
+        return menu.size();
+    }
+}
+
+class MenuViewHolder extends RecyclerView.ViewHolder  {
+
+    public TextView txtMenu;
+    public ImageView img;
+    RelativeLayout relativeLayout;
+    //private ItemClickListener itemClickListener;
+
+    public MenuViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtMenu=itemView.findViewById(R.id.menu_namae);
+        img=itemView.findViewById(R.id.menu_list);
+        relativeLayout = itemView.findViewById(R.id.parent_layout);
+
+        //itemView.setOnClickListener(this);
+
+    }
+
+   /* public void setItemClickListener(ItemClickListener itemClickListener){
+        this.itemClickListener=itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(itemClickListener!=null) {
+            itemClickListener.onClick(v, getAdapterPosition(), false);
+        }
+    }*/
+}
---
+++ app/src/main/java/com/example/laharish/MainMenu.java
@@ -0,0 +1,154 @@
+package com.example.laharish;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.laharish.Interface.ItemClickListener;
+import com.example.laharish.Model.Category;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.view.View.OnClickListener;
+
+public class MainMenu extends AppCompatActivity {
+
+    //Getting reference from database
+    FirebaseDatabase fireData;
+    DatabaseReference ref;
+
+    //calling adapter object
+    ImageAdapter mAdapter;
+
+    RecyclerView recycler_menu;
+    RecyclerView.LayoutManager layoutManager;
+
+    private List<Category> cat;
+
+    Toolbar toolbar;
+
+    String key,TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Log.d(TAG, "onCreate: started");
+
+        fireData = FirebaseDatabase.getInstance();
+        ref = fireData.getReference("Menu");
+
+        toolbar = findViewById(R.id.toolbarMain);
+        toolbar.setTitle("MENU");
+
+        recycler_menu = findViewById(R.id.recycler_menu);
+        recycler_menu.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recycler_menu.setLayoutManager(layoutManager);
+
+        //Creation of array of objects to store the data
+        cat = new ArrayList<>();
+
+        //to put the data into the above array from firebase
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    final Category cate = postSnapshot.getValue(Category.class);
+                    cat.add(cate);
+                }
+
+                mAdapter = new ImageAdapter(MainMenu.this, cat);
+                recycler_menu.setAdapter(mAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainMenu.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //To take the user to the cart
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Going to cart", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+}
+/*
+// To display the data onto the screen
+class ImageAdapter extends RecyclerView.Adapter<MenuViewHolder> {
+
+    private Context mContext;
+    private List<Category> menu;
+
+    public ImageAdapter(Context context, List<Category> menus){
+        mContext=context;
+        menu=menus;
+    }
+
+    @NonNull
+    @Override
+    public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v= LayoutInflater.from(mContext).inflate(R.layout.menu_item, viewGroup, false);
+        return new MenuViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MenuViewHolder menuViewHolder, final int i) {
+        final Category mmenu=menu.get(i);
+        menuViewHolder.txtMenu.setText(mmenu.getName());
+        Picasso.with(mContext).load(mmenu.getImage())
+                .fit()
+                .centerCrop()
+                .into(menuViewHolder.img);
+
+        menuViewHolder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                Toast.makeText(mContext, ""+mmenu.getName(), Toast.LENGTH_SHORT).show();
+                Intent foodList = new Intent(mContext, FoodDetail.class);
+                foodList.putExtra("CategoryID", ref.getRef().getKey()) ;
+                view.getContext().startActivity(foodList);}
+
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return menu.size();
+    }
+}
+}
+*/
+
+
+
+
+
+
